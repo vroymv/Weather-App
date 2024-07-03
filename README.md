c859cf0c1b44506b9efaf9fc843f47828437de9e
Mbong Vanistelroy <119032290+vroymv@users.noreply.github.com>
2024-07-03 14:24:37 +0530

    update readme.md

---
+++ README.md
@@ -0,0 +1,102 @@
+<!-- Banner Image, Landing Page Of Computer Vision Site -->
+<div align="center">
+  <br />
+    <a href="">
+
+  <div>
+        <img src="https://img.shields.io/badge/node-js?style=for-the-badge&logo=nodedotjs&logoColor=white&label=Node%20JS" alt="Node JS" />
+
+  </div>
+
+  <h1 align="center">Weather App</h1>
+</div>
+
+## 📋 <a name="table">Table of Contents</a>
+
+1. 🤖 [Introduction](#introduction)
+2. ⚙️ [Tech Stack](#tech-stack)
+3. 🔋 [Features](#features)
+4. 🤸 [Quick Start](#quick-start)
+5. 🕸️ [Snippets](#snippets)
+
+## 🚨 About
+
+Enter a City and get it's current weather conditions. (Rain and temperature)
+
+## <a name="introduction">🤖 Introduction</a>
+
+This simple project implements node js and open weather map api
+
+## <a name="tech-stack">⚙️ Tech Stack</a>
+
+- Node js
+- Open Weather map api.
+
+## <a name="features">🔋 Features</a>
+
+👉 **Home Screen**:
+
+<img src="https://firebasestorage.googleapis.com/v0/b/karizmatik-14de4.appspot.com/o/Screenshot%202024-07-03%20at%2014.02.26.png?alt=media&token=a12ad4ff-a56f-40ff-b54e-55df03a875c5">
+
+👉 **Weather Result**:
+
+<img src="https://firebasestorage.googleapis.com/v0/b/karizmatik-14de4.appspot.com/o/Screenshot%202024-07-03%20at%2014.02.41.png?alt=media&token=88a45ee1-2672-4901-a06d-9ddf95596cdf" alt="Weather Results">
+
+## <a name="quick-start">🤸 Quick Start</a>
+
+Follow these steps to set up the project locally on your machine.
+
+**Prerequisites**
+
+Make sure you have the following installed on your machine:
+
+- [Git](https://git-scm.com/)
+- [Node.js](https://nodejs.org/en)
+- [npm](https://www.npmjs.com/) (Node Package Manager)
+
+**Cloning the Repository**
+
+```bash
+git clone https://github.com/vroymv/Weather-App.git
+cd Weather-App
+```
+
+**Installation**
+
+Install the project dependencies using npm:
+
+```bash
+npm install
+```
+
+**Running the Project**
+
+```bash
+nodemon app.js
+```
+
+**Setup Open Weather Map API**
+
+Create An Account on Open Weather Map <br>
+I useed this api, feel free to use any other
+https://openweathermap.org/<br>
+
+## <a name="snippets">🕸️ Snippets</a>
+
+**Modify the following In your Code**
+
+<details>
+<summary><code>app.js</code></summary>
+
+In the code below, replace ${process.env.apiid} with your actual api key.
+
+```javascript
+https
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=${process.env.apiid}&lang=en`,
+      function (response) {
+        console.log("Status Code: " + response.statusCode);
+
+```
+
+</details>
--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //Requiring the different needed packages
-const express = require('express');
-const https = require('https');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
 
 //Declaring global variables
 var hexJson;
@@ -14,53 +15,52 @@ const app = express();
 //Making use of body-parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 //Making a get request to open weather api
-app.get('/', function(req, res){
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 
+  // res.setHeader('Content-Type', 'text/html');
 
-
-    res.sendFile(__dirname + '/index.html');
-
-    // res.setHeader('Content-Type', 'text/html');
-
-    // const imageUrl = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';
-    // res.write('The temperatures in London are: ' + temp  + '<br/>');
-    // res.write('The weather is ' + description);
-    // res.write(' <img src="' + imageUrl + '"/>');
-})
+  // const imageUrl = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';
+  // res.write('The temperatures in London are: ' + temp  + '<br/>');
+  // res.write('The weather is ' + description);
+  // res.write(' <img src="' + imageUrl + '"/>');
+});
 
 //Post request
-app.post('/',function(req, res){
+app.post("/", function (req, res) {
+  var city = req.body.city;
 
-    var city = req.body.city;
+  https
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=${process.env.apiid}&lang=en`,
+      function (response) {
+        console.log("Status Code: " + response.statusCode);
 
-    https.get('https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=51bc7a453243fb5bba48d91ff1e8d215&lang=en', function(response){
-        console.log('Status Code: ' + response.statusCode);
-    
-        response.on('data', function(d){
-            hexJson = JSON.parse(d);
-            //console.log(hexJson);
-            temp = (hexJson.main.temp);
-            description = hexJson.weather[0].description;
-            icon = hexJson.weather[0].icon;
-        })
-    }).on('error', function(e){
-        console.log(e);
-    })
+        response.on("data", function (d) {
+          hexJson = JSON.parse(d);
+          console.log(hexJson);
+          temp = hexJson?.main?.temp;
+          description = hexJson?.weather[0]?.description;
+          icon = hexJson?.weather[0]?.icon;
+        });
+      }
+    )
+    .on("error", function (e) {
+      console.log(e);
+    });
 
+  res.setHeader("Content-Type", "text/html");
 
-        res.setHeader('Content-Type', 'text/html');
+  const imageUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+  res.write("The temperature in" + city + "is: " + temp + "<br/>");
+  res.write("The weather is " + description);
+  res.write(' <img src="' + imageUrl + '"/>');
 
-    const imageUrl = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';
-    res.write('The temperature in'+city+ 'is: ' + temp  + '<br/>');
-    res.write('The weather is ' + description);
-    res.write(' <img src="' + imageUrl + '"/>');
+  console.log(req.body.city);
+  res.send();
+});
 
-    console.log(req.body.city);
-    res.send();
-})
-
-app.listen(3000, function(){
-    console.log('Server successfully started');
-})
+app.listen(3000, function () {
+  console.log("Server successfully started");
+});
